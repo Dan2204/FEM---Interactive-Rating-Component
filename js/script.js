3edba2378184f89f4aa96b388ba072128f2de6b4
Dan Lucas <dan.lucas2204@gmail.com>
2022-06-14 20:00:05 +0100

    improved js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const options = document.querySelectorAll('.option');
 const btnSubmit = document.getElementById('submit');
 const selected = document.getElementById('selected');
-const rate = document.getElementById('rate');
-const thanks = document.getElementById('thanks');
+const cards = document.querySelectorAll('.card');
+
 let selectedRating = 0;
 
 options.forEach((option) => {
   option.addEventListener('click', (e) => {
-    options.forEach((opt) => {
-      opt.classList.remove('active');
-    });
+    if (selectedRating > 0) {
+      document
+        .querySelector(`.options :nth-child(${selectedRating})`)
+        .classList.remove('active');
+    }
     option.classList.add('active');
     selectedRating = e.target.textContent;
   });
@@ -17,8 +19,6 @@ options.forEach((option) => {
 
 btnSubmit.addEventListener('click', () => {
   if (selectedRating < 1) return;
-  rate.classList.add('hidden');
-  thanks.classList.remove('hidden');
+  cards.forEach((card) => card.classList.toggle('hidden'));
   selected.textContent = selectedRating;
-  console.log(`Click! - ${selectedRating} selected`);
 });
